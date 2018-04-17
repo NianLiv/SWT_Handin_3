acbaf0e7340f4a452dfe499ba1069e3f83e19c58
NianLiv <au564992@uni.au.dk>
2018-04-17 09:14:25 +0200

    Oprettet generic observer pattern

---
+++ AirTrafficMonitoring/AirTrafficMonitoring.Lib/Subject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficMonitoring.Lib
+{
+    public abstract class Subject<T>
+    {
+        private List<IObserver<T>> _observers = new List<IObserver<T>>();
+
+        public void Attach(IObserver<T> observer)
+        {
+            if (!_observers.Contains(observer))
+                _observers.Add(observer);
+        }
+
+        public void Detach(IObserver<T> observer)
+        {
+            if (_observers.Contains(observer))
+                _observers.Remove(observer);
+        }
+
+        public void Notify(T obj)
+        {
+            foreach (var sub in _observers)
+                sub.Update(obj);
+        }
+    }
+}
