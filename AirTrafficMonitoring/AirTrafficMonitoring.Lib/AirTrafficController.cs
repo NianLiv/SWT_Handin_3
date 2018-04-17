2a49e4fc94b466b78cae3d4cdbbbb8be17d20765
NianLiv <au564992@uni.au.dk>
2018-04-17 09:24:09 +0200

    Ting med controller

--- AirTrafficMonitoring/AirTrafficMonitoring.Lib/AirTrafficController.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring.Lib/AirTrafficController.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirTrafficMonitoring.Lib.Interfaces;
 
 namespace AirTrafficMonitoring.Lib
 {
     public class AirTrafficController : IObserver<Tos>
     {
+        private ICollisionDetector _collisonDetector;
+        private ITrackStorage _trackStorage;
+        private IAirSpace _airSpace;
+        private IRender _render;
+        private ILog _log;
+
+        public AirTrafficController(ICollisionDetector cd, ITrackStorage ts, IAirSpace airs, IRender r, ILog l)
+        {
+            _collisonDetector = cd;
+            _trackStorage = ts;
+            _airSpace = airs;
+            _render = r;
+            _log = l;
+        }
 
         public void Update(Tos obj)
         {
