1b95fb7ddbd56566202e8fb8dfc8d08eec2cebb9
NianLiv <au564992@uni.au.dk>
2018-04-18 12:27:44 +0200

    AirTrafficController nu med eventhandler der kaldes på collision event og printer listen af collision tracks.

--- AirTrafficMonitoring/AirTrafficMonitoring.Lib/Interfaces/IRender.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring.Lib/Interfaces/IRender.cs
@@ -9,5 +9,6 @@ namespace AirTrafficMonitoring.Lib.Interfaces
     public interface IRender
     {
         void PrintTrackData(List<Track> tracks);
+        void PrintCollisionTracks(List<CollisionPairs> pairs);
     }
 }
