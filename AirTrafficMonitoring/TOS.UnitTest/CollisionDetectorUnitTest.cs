7dd2c86fbaf0aa8a145cb8fd29d1fc4d592c5ac2
RasmusThorsen <rallethor@hotmail.com>
2018-04-19 10:03:21 +0200

    Tilføjet CollisionDetector unittest

--- AirTrafficMonitoring/TOS.UnitTest/CollisionDetectorUnitTest.cs
+++ AirTrafficMonitoring/TOS.UnitTest/CollisionDetectorUnitTest.cs
@@ -14,7 +14,6 @@ namespace TOS.UnitTest
     class CollisionDetectorUnitTest
     {
         private ICollisionDetector _uut;
-        private CollisionEventArgs _fakeArgs;
         private int SeparationEventCounter = 0;
         private int NotCollidingEventCounter = 0;
 
@@ -23,13 +22,69 @@ namespace TOS.UnitTest
         {
             _uut = new CollisionDetector();
 
+            //Attaching event handlers
             _uut.Separation += (e, args) => SeparationEventCounter++;
             _uut.NotColliding += (e, args) => NotCollidingEventCounter++;
         }
 
-        public void SeparationEventCalled()
+        [Test]
+        public void CheckForCollision_SeparationEventRaised_SeparationCounterIsTwo()
         {
-            _uut.CheckForCollision(null);
+            //Reset event counters
+            SeparationEventCounter = 0;
+            NotCollidingEventCounter = 0;
+            //Add colliding tracks to list
+            var tracks = new List<ITrack>
+            {
+                new Track{PositionX = 5000, PositionY = 5000, Tag = "AAA", Timestamp = DateTime.Now, Altitude = 1000},
+                new Track{PositionX = 5000, PositionY = 5000, Tag = "BBB", Timestamp = DateTime.Now, Altitude = 1000},
+            };
+
+            _uut.CheckForCollision(tracks);
+
+            //Is equal to two because both tracks cause the event.
+            Assert.That(SeparationEventCounter, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void CheckForCollision_SeparationEventNotRaised_SeparationEventCounterIsZero()
+        {
+            //Reset Event counters
+            SeparationEventCounter = 0;
+
+            //Add non colliding tracks to a list
+            var tracks = new List<ITrack>
+            {
+                new Track{PositionX = 50000, PositionY = 15000, Tag = "AAA", Timestamp = DateTime.Now, Altitude = 9000},
+                new Track{PositionX = 5000, PositionY = 5000, Tag = "BBB", Timestamp = DateTime.Now, Altitude = 1000},
+            };
+
+            _uut.CheckForCollision(tracks);
+
+            //Is equal to two because both tracks cause the event.
+            Assert.That(SeparationEventCounter, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CheckForCollision_NotCollidingEventRaised_NotCollidingEventCounterIsTwo()
+        {
+            NotCollidingEventCounter = 0;
+
+            //Add colliding tracks
+            var tracks = new List<ITrack>
+            {
+                new Track{PositionX = 5000, PositionY = 5000, Tag = "AAA", Timestamp = DateTime.Now, Altitude = 1000},
+                new Track{PositionX = 5000, PositionY = 5000, Tag = "BBB", Timestamp = DateTime.Now, Altitude = 1000},
+            };
+            _uut.CheckForCollision(tracks);
+
+            //Move a track out of collision zone
+            tracks[0].Altitude = 9000;
+
+            _uut.CheckForCollision(tracks);
+
+            //Is equal to two because both tracks cause the event.
+            Assert.That(NotCollidingEventCounter, Is.EqualTo(2));
         }
     }
 }
