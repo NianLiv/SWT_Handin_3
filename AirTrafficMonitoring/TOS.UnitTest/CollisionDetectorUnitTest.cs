a91eeeff5ee3f4b7e04e0f58fb628b82465a7d97
RasmusThorsen <rallethor@hotmail.com>
2018-04-19 09:30:52 +0200

    unittestttt

---
+++ AirTrafficMonitoring/TOS.UnitTest/CollisionDetectorUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitoring.Lib.Interfaces;
+using AirTrafficMonitoring.Lib;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace TOS.UnitTest
+{
+    [TestFixture]
+    class CollisionDetectorUnitTest
+    {
+        private ICollisionDetector _uut;
+        private CollisionEventArgs _fakeArgs;
+        private int SeparationEventCounter = 0;
+        private int NotCollidingEventCounter = 0;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new CollisionDetector();
+
+            _uut.Separation += (e, args) => SeparationEventCounter++;
+            _uut.NotColliding += (e, args) => NotCollidingEventCounter++;
+        }
+
+        public void SeparationEventCalled()
+        {
+            _uut.CheckForCollision(null);
+        }
+    }
+}
