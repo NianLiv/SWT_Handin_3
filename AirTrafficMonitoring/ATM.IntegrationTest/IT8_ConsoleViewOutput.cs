51d331bc58aff60cb544954a1db7ae44736383a4
Nicolai Andersen <n.andersen758@gmail.com>
2018-04-22 14:40:18 +0200

    IT8 ConsoleView Output done

--- AirTrafficMonitoring/ATM.IntegrationTest/IT8_ConsoleViewOutput.cs
+++ AirTrafficMonitoring/ATM.IntegrationTest/IT8_ConsoleViewOutput.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using AirTrafficMonitoring.Lib;
 using AirTrafficMonitoring.Lib.Interfaces;
 using NSubstitute;
-using NSubstitute.Extensions;
 using NUnit.Framework;
 
 namespace ATM.IntegrationTest
@@ -15,6 +14,7 @@ namespace ATM.IntegrationTest
         private ConsoleView _consoleView;
         private CollisionPairs _collisionPairs;
         private List<ITrack> _validTracks;
+        private List<CollisionPairs> _validCollisionPairs;
 
         [SetUp]
         public void SetUp()
@@ -70,10 +70,11 @@ namespace ATM.IntegrationTest
                     Altitude = 2900
                 }
             };
+            _validCollisionPairs = new List<CollisionPairs> {_collisionPairs};
         }
 
         [Test]
-        public void PrintTrackData_ClearIsCalled_OutputRecievesEmptyStringTwoThirdsOfHeightTimes()
+        public void PrintTrackData_ClearIsCalled_OutputReceivesEmptyStringTwoThirdsOfHeightTimes()
         {
             _consoleView.PrintTrackData(_validTracks);
             var clearHeight = (_consoleView.Height - _consoleView.Height / 3);
@@ -82,12 +83,76 @@ namespace ATM.IntegrationTest
         }
 
         [Test]
-        public void PrintTrackDate_RenderMapIsCalledWithListCount3_OutputRecievesCharXThreeTimes()
+        public void PrintTrackData_RenderMapIsCalledWithListCount3_OutputReceivesCharXThreeTimes()
         {
             _consoleView.PrintTrackData(_validTracks);
             _output.Received(3).OutputLine("x");
         }
 
+        [Test]
+        public void PrintTrackData_ValidList_OutputReceivesCursorPositionChange()
+        {
+            _consoleView.PrintTrackData(_validTracks);
+            _output.ReceivedWithAnyArgs().SetCursorPosition(int.MaxValue, int.MaxValue);
+        }
+
+        [Test]
+        public void PrintTrackData_RenderTrackData_OutputReceivesAllTrackTags()
+        {
+            _consoleView.PrintTrackData(_validTracks);
+
+            foreach (var track in _validTracks)
+                _output.Received(1).OutputLine(Arg.Is<string>(x => x.Contains(track.Tag)));
+        }
+
+        [Test]
+        public void SetUpConsole_OutputReceivesGetLargestScreenSize()
+        {
+            // ConsoleView ctor is called in SetUp().
+            _output.Received(1).GetLargetsScreenSize(out var w, out var h);
+        }
+
+        [Test]
+        public void SetUpConsole_OutputReceivedGetLargesScreenSize_ViewWidthIs80()
+        {
+            // ConsoleView ctor is called in SetUp().
+            Assert.That(_consoleView.Width, Is.EqualTo(80));
+        }
+
+        [Test]
+        public void SetUpConsole_OutputReceivedGetLargestScreenSize_ViewHeightIs40()
+        {
+            // ConsoleView ctor is called in SetUp().
+            Assert.That(_consoleView.Height, Is.EqualTo(40));
+        }
+
+        [Test]
+        public void SetUpConsole_ArgHeigtAndWidth_SetWindowSizeWithWidthAndHeight()
+        {
+            // ConsoleView ctor is called in SetUp().
+            _output.Received(1).SetWindowSize(_consoleView.Width, _consoleView.Height);
+        }
+
+        [Test]
+        public void PrintCollisionTracks_OutputReceivesSetCursorPosition()
+        {
+            _consoleView.PrintCollisionTracks(_validCollisionPairs);
+            _output.ReceivedWithAnyArgs().SetCursorPosition(0,0);
+        }
+
+        [Test]
+        public void PrintCollisionTracks_ValidCollisionsListCount1_OutputReceivesCurrentTrackTag()
+        {
+            _consoleView.PrintCollisionTracks(_validCollisionPairs);
+            _output.Received(1).OutputLine(Arg.Is<string>(x => x.Contains(_validCollisionPairs[0].currentTrack.Tag)));
+        }
+
+        [Test]
+        public void PrintCollisionTracks_ValidCollisionsListCount1_OutputReceivesotherTrackTag()
+        {
+            _consoleView.PrintCollisionTracks(_validCollisionPairs);
+            _output.Received(1).OutputLine(Arg.Is<string>(x => x.Contains(_validCollisionPairs[0].otherTrack.Tag)));
+        }
 
     }
 
