44353ac82f2cde7c4df94e3eb2ed7cecb27164f2
RasmusThorsen <rallethor@hotmail.com>
2018-04-22 11:49:38 +0200

    IT5 + IT7

---
+++ AirTrafficMonitoring/ATM.IntegrationTest/IT7_CollisionDetector.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitoring.Lib;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace ATM.IntegrationTest
+{
+    [TestFixture]
+    class IT7_CollisionDetector
+    {
+        private CollisionDetector _detector;
+        private List<ITrack> TrackList;
+        private bool wasCalled = false;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _detector = new CollisionDetector();
+            Track _track1 = new Track
+            {
+                Tag = "AAA111",
+                PositionX = 15000,
+                PositionY = 30000,
+                Altitude = 850,
+                Course = 0,
+                Timestamp = DateTime.Now,
+                Velocity = 0
+            };
+            TrackList.Add(_track1);
+
+            Track _track2 = new Track
+            {
+                Tag = "AAA111",
+                PositionX = 15000,
+                PositionY = 30000,
+                Altitude = 850,
+                Course = 0,
+                Timestamp = DateTime.Now,
+                Velocity = 0
+            };
+            TrackList.Add(_track2);
+
+            _detector.Separation += (o, e) => wasCalled = true;
+            _detector.NotColliding += (o, e) => wasCalled = true;
+        }
+
+        [Test]
+        public void CheckForCollisions_CollidingTrack_CallingSeparationEvent()
+        {
+            _detector.CheckForCollision(TrackList);
+            Assert.That(wasCalled, Is.EqualTo(true));
+
+        }
+    }
+}
