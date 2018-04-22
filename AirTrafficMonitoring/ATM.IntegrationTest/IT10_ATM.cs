d9cc893f3e6e179fc72d47c06f178ef54f446948
Kristoffer Stampe Villadsen <Villadsen67@hotmail.com>
2018-04-22 15:42:02 +0200

    Added IT10 for testing

---
+++ AirTrafficMonitoring/ATM.IntegrationTest/IT10_ATM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitoring.Lib;
+using AirTrafficMonitoring.Lib.Interfaces;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace ATM.IntegrationTest
+{
+    [TestFixture]
+    class IT10_ATM
+    {
+        private CollisionDetector _collisionDetector;
+        private TrackStorage _trackStorage;
+        private AirSpace _airSpace;
+        private ConsoleView _consoleView;
+        private FileLogger _log;
+        private IOutput _output;
+
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _collisionDetector = new CollisionDetector();
+            _trackStorage = new TrackStorage();
+            _airSpace = new AirSpace(90000, 10000, 90000, 10000, 500, 20000);
+            _consoleView = new ConsoleView(_output);
+            _log = new FileLogger(@"CollisionATMTest.txt");
+        }
+
+
+    }
+}
